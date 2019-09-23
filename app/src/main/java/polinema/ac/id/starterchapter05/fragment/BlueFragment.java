7e4e0781a5b6387770b946f2d8f4589444974499
Rivaldz <rivalzsetyo@gmail.com>
2019-09-23 15:01:56 +0700

    create 2 fragment in 1 activity

--- app/src/main/java/polinema/ac/id/starterchapter05/fragment/BlueFragment.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragment/BlueFragment.java
@@ -15,26 +15,17 @@ import polinema.ac.id.starterchapter05.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RedFragment.OnFragmentInteractionListener} interface
+ * {@link BlueFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RedFragment#newInstance} factory method to
+ * Use the {@link BlueFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RedFragment extends Fragment {
-    public RedFragment() {
-        // Required empty public constructor
-    }
-
+public class BlueFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        return inflater.inflate(R.layout.fragment_blue, container, false);
     }
-}
-
-
-
-
-
+ }
