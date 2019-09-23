71748ec9fcbddd3d8cd54122178ed30b44a66b3d
Rivaldz <rivalzsetyo@gmail.com>
2019-09-23 14:02:21 +0700

    create and changes fragment_red.xml

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activity.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_praktikum_fragment(View view) {
+    }
+
+    public void btn_dynamic_fragment(View view) {
+    }
+
+    public void btn_static_fragment(View view) {
+        Intent i = new Intent(this, StaticActivity.class);
+        startActivity(i);
     }
 }
